5d55a2dfee8aa76cdd78df3ab50249d2f1f23ecf
Neil Lopes <127422402+PhArObiS@users.noreply.github.com>
2024-10-19 15:28:56 -0400

    Added Team to title

--- components/sub/AboutContent.tsx
+++ components/sub/AboutContent.tsx
@@ -28,7 +28,7 @@ const AboutContent = () => {
         >
           <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
           <h1 className="Welcome-text text-[13px]">
-            FORTNITE CREATIVE UEFN
+            FORTNITE CREATIVE UEFN TEAM
           </h1>
         </motion.div>
 
