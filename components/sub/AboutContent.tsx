e9205cb2219191762569aba87f812c612683cba0
Neil Lopes <127422402+PhArObiS@users.noreply.github.com>
2026-02-11 16:37:04 -0500

    Join our communty

--- components/sub/AboutContent.tsx
+++ components/sub/AboutContent.tsx
@@ -52,6 +52,7 @@ const AboutContent = () => {
           <p className="mb-6">
             Whether you&apos;re looking for custom maps, game modes, or specialized effects, our team combines technical expertise with artistic vision to deliver top-tier content that stands out. Join us on this creative journey and explore the limitless potential of UEFN with us.
 
+            Join our community!
             https://www.fortnite.com/@nldevs
           </p>
         </motion.div>
