7db823ad43e3e0752f8072a37928be24518510a1
Neil Lopes <127422402+PhArObiS@users.noreply.github.com>
2026-02-11 23:00:07 -0500

    Removed  h2 tag

--- components/main/FortniteMaps.tsx
+++ components/main/FortniteMaps.tsx
@@ -5,9 +5,9 @@ import FortniteMapsCard from "../sub/FortniteMapsCard";
 const FortniteMaps = () => {
   return (
     <div className="flex flex-col items-center justify-center py-20" id="fortnitemaps">
-      <h2 className="text-[40px] font-mono font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20">
+      {/* <h2 className="text-[40px] font-mono font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20">
       FAVORITE FORTNITE MAPS
-      </h2>
+      </h2> */}
       <div className="font-mono h-full w-full lg:grid lg:grid-cols-3 lg:gap-10 px-10">
         <FortniteMapsCard
           src="/MegaRampSurvival.jpeg"
