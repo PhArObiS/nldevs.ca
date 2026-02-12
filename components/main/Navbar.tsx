1df1a3e99a6c8ef0c8030fd2d095f0c80fe4d398
Neil Lopes <127422402+PhArObiS@users.noreply.github.com>
2026-02-11 22:26:39 -0500

    Navbar

--- components/main/Navbar.tsx
+++ components/main/Navbar.tsx
@@ -21,7 +21,7 @@ const Navbar = () => {
           </a>
 
           {/* Adjusted navbar section */}
-          <div className="flex flex-row items-center justify-between w-full max-w-[500px] h-full md:mr-20">
+          {/* <div className="flex flex-row items-center justify-between w-full max-w-[500px] h-full md:mr-20">
             <div className="flex items-center justify-between w-full h-auto border border-[#7042f861] bg-[#0300145e] mr-[15px] px-[20px] py-[10px] text-gray-200 ml-[15px]">
               <a href="" className="cursor-pointer">
                 Fortnite
@@ -33,7 +33,7 @@ const Navbar = () => {
                 Team
               </a>
             </div>
-          </div>
+          </div> */}
 
           {/* Social Icons with Links */}
           <div className="flex flex-row gap-2">
