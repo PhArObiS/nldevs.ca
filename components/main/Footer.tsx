3c3819d562666024e18a7d89ace1f1a205cc1f12
Neil Lopes <127422402+PhArObiS@users.noreply.github.com>
2026-02-12 17:14:52 -0500

    Under construction

--- components/main/Footer.tsx
+++ components/main/Footer.tsx
@@ -51,7 +51,7 @@ const Footer: React.FC = () => (
       <Section title="About" links={aboutLinks} /> */}
     </div>
     <div className="text-center text-sm text-gray-400 mt-6">
-      &copy; NLDEVS 2026
+      &copy; NLDEVS 2026 - Under Construction
     </div>
   </footer>
 );
